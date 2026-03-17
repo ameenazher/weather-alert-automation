5155b16e28d73d862118c475b2cb58befcbdeb88
ameen azher <ameen.azher@icloud.com>
2026-03-17 20:32:40 +0300

    feat: add WhatsApp weather alert using Twilio API
    Integrated OpenWeatherMap API to fetch real-time weather data and detect rain conditions.
    
    Added Twilio WhatsApp messaging to send alerts when rain is detected based on:
    - Rain volume
    - Weather condition codes
    
    Configured environment variables for secure API usage.

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
+import requests
+import os
+from twilio.rest import Client
 
+# 🔐 Secrets
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+# 📍 Baghdad
+lat = 33.314690
+lon = 44.376759
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+params = {
+    "lat": lat,
+    "lon": lon,
+    "appid": api_key,
+    "units": "metric"
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code != 200:
+    print("❌ API request failed")
+    exit()
+
+data = response.json()
+
+if "main" in data:
+    temp = data["main"]["temp"]
+    weather = data["weather"][0]["description"]
+    weather_id = data["weather"][0]["id"]
+    rain = data.get("rain", {}).get("1h", 0)
+
+    city = data["name"]
+
+    message_text = f"""
+🌧️ Weather Alert - {city}
+
+🌡️ Temp: {temp}°C
+🌥️ Condition: {weather}
+🌧️ Rain: {rain} mm
+"""
+
+    print(message_text)
+
+    # 🚨 Send only if rain detected
+    if rain > 0 or (200 <= weather_id < 600):
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            from_="whatsapp:+14155238886",
+            body=message_text,
+            to="whatsapp:+964XXXXXXXXXX"
+        )
+
+        print("✅ WhatsApp Message Sent!")
+    else:
+        print("☀️ No rain → No message sent")
+
+else:
+    print("❌ API Error:", data)
